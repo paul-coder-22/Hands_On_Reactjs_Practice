9df098a89b78c0f75c913aab2432a2a2d42e8550
P@ul <MyEmail@Zoho.com>
2021-11-19 01:42:26 +0530

    feat - UI updated of tabs project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
+
+  const [loading, getApipading] = useState(true);
+  const [jobs, getJobs] = useState([]);
+  const [value, getValue] = useState(0);
+  const fecthValue = async () => {
+    const apiValue = await fetch('https://course-api.com/react-tabs-project');
+    const jsonValue = await apiValue.json();
+    getJobs(jsonValue);
+    getApipading(false)
+  }
+
+  useEffect(() => {
+    fecthValue();
+  }, [])
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h2>loading...</h2>
+      </section>);
+  }
+  const { company, dates, duties, title } = jobs[value];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section >
+  )
 }
 
 export default App;
